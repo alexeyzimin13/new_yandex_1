0f57bc07792c9ae52a541dfa9c1004b98cc3cb46
Alexey Zimin <zimin.e-mail@yandex.ru>
2022-12-18 19:05:40 +0300

    second

--- src/test/java/TestTwo.java
+++ src/test/java/TestTwo.java
@@ -6,7 +6,6 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-
 import java.util.concurrent.TimeUnit;
 
 public class TestTwo {
